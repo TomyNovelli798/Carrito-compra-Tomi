98de50588f154f1d738bba25e608a9d7df378a27
CarritoTomi <endertomi@gmail.com>
2022-09-15 16:52:41 -0300

    carrito

---
+++ main JavScript notas.js
@@ -0,0 +1,81 @@
+//let textoA = "CODER";
+//let textoB = "HOUSE";
+
+//const BLANCO = " ";
+
+//let resultado = textoA + textoB;
+
+//let resultadoB = textoB + 1;
+//console.log(resultadoB);
+
+//let resultadoC = textoA + BLANCO + textoB;
+//console.log(resultadoC)
+
+//let nombreIngresado = prompt("ingrese su nombre");
+//console.log(nombreIngresado)
+
+//let numero1 = parseInt(prompt("Ingrese un numero:"));
+//let numero2 = parseInt(prompt("Ingrese otro numero:"));
+
+//let resultado = numero1 + numero2;
+//console.log(resultado);
+
+//let numeroElsef = parseInt(prompt("ingresa un numero del 0 al 100"));
+
+//if (numeroElself> 100){
+//    alert ("el numero esta fuera del rango");
+//} else if (numeroElsef < 0){
+//    alert("el numero esta fuera del rango");
+//} else {
+//    alert("el numero esta dentro del rango");
+//}
+
+//let numero = 4 // = operador de asignación
+//if ( numero == 3) {
+//    console.log ("Es verdadero")
+//} else{
+//    console.log("Es falso")
+//}
+
+// let nombreDeUsuario = prompt("Ingrese su nombre de usuario");
+
+// if (nombreDeUsuario == "") {
+//     alert("No ingresaste un nombre de usuario!");
+// } else {
+//     alert ("Nombre de usuario ingresado:" + nombreDeUsuario);
+// }
+
+//TRUE O FALSE
+//let numero = 10;
+//let esMayorQue5 = numero > 5
+
+// if (esMayorQue5) {
+    //alert ("Efectivamente" + "numero + es mayor que 5")
+//} else {
+    //alert(numero + "No es mayor que 5")
+//} cambiar el 10 para que salte el segundo alert
+
+// let nombreIngresado = prompt("Ingresar nombre")
+// let apellidoIngresado = prompt("Ingresar apellido")
+
+// if((nombreIngresado != "") && (apellidoIngresado != "")){
+//     alert("Nombre:" +nombreIngresado+ "/nApellido:" +apellidoIngresado );
+// } else{
+//     alert("Error: Falta alguno de los datos!")
+// }
+
+//let nombreIngresado = prompt("Ingrese su nombre:");
+
+// if((nombreIngresado == "ANA") || (nombreIngresado == "ana")) {
+//     alert ("El nombre ingresado es Ana");
+// } else{
+//     alert("El nombre ingresado No es Ana")
+// }
+// let entrada = prompt("ingrese una pelicula");
+
+// while(entrada != "ESC") {
+//     alert("El usuario ingreso" + entrada);
+//     entrada = prompt("Ingrese otra pelicula");
+// }
+
+//PRACTICA
