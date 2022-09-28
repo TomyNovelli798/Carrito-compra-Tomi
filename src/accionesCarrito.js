1ca027edcf1449e3de2308f96ebbba151573b7be
CarritoTomi <endertomi@gmail.com>
2022-09-28 15:42:45 -0300

    avanzado

---
+++ src/accionesCarrito.js
@@ -0,0 +1,49 @@
+import { actualizarTotalesCarrito } from "./actualizarCarrito.js";
+import { productos } from "./stock.js";
+
+let carrito = [];
+
+const validarProductoCarrito = (productoId) => {
+    const productoRepetido = carrito.find(producto => producto.id === productoId);
+
+    if (productoRepetido) {
+        productoRepetido.cantidad++;
+        const cantidadProducto = document.getElementById(`cantidad${productoRepetido.id}`);
+        cantidadProducto.innerText = `Cantidad: ${productoRepetido.cantidad}`
+        actualizarTotalesCarrito(carrito);
+    } else {
+        agregarAlCarrito(productoId);
+    }
+};
+
+const agregarAlCarrito = (productoId) => {
+    const contenedor = document.getElementById('carrito-contenedor');
+    const producto = productos.find(producto => producto.id === productoId);
+    carrito.push(producto);
+
+    const div = document.createElement('div');
+    div.classList.add('productoEnCarrito');
+    div.innerHTML = `<p>${producto.nombre}</p>
+                    <p>Precio: ${producto.precio}</p>
+                    <p id=cantidad${producto.id}>Cantidad: ${producto.cantidad}</p>
+                    <button id=eliminar${producto.id} class='btn waves-effect waves-ligth boton-eliminar' value='${producto.id}'>X</button>
+    `;
+    contenedor.appendChild(div);
+    actualizarTotalesCarrito(carrito);
+};
+
+const pintarCarrito = (carrito) => {
+    const contenedor = document.getElementById('carrito-contenedor');
+
+    carrito.forEach(producto => {
+        const div = document.createElement('div');
+        div.classList.add('productoEnCarrito');
+        div.innerHTML = `<p>${producto.nombre}</p>
+                        <p>Precio: ${producto.precio}</p>
+                        <p id=cantidad${producto.id}>Cantidad: ${producto.cantidad}</p>
+                        <button id=eliminar${producto.id} class='btn waves-effect waves-ligth boton-eliminar' value='${producto.id}'>X</button>
+        `;
+        contenedor.appendChild(div);
+    });
+};
+export { validarProductoCarrito, pintarCarrito };
---
+++ src/stock.js
@@ -0,0 +1,55 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Reloj 1",
+        tipo: "Reloj",
+        desc: "Reloj Rolex",
+        precio: 195000,
+        img: '',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Collar 1",
+        tipo: "Collar",
+        desc: "Collar de acero inoxidable",
+        precio: 4500,
+        img: '',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Anillo 1",
+        tipo: "Anillo",
+        desc: "Anillo de oro con grabados",
+        precio: 900,
+        talle: "19mm",
+        img: '',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Lentes 1",
+        tipo: "Lentes",
+        desc: "Lentes Hugo Boss",
+        precio: 14500,
+        img: '',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Aros 2",
+        tipo: "Aros",
+        desc: "Aros de plata",
+        precio: 2000,
+        img: '',
+        cantidad:1
+    },
+];
+
+export { productos };
